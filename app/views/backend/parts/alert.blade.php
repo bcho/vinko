c056c34f643fb0c28c6a9e7a090d8dd98ffbd606
hbc <bcxxxxxx@gmail.com>
2013-11-16 15:42:39 +0800

    fixed typo

--- app/views/backend/parts/alert.blade.php
+++ app/views/backend/parts/alert.blade.php
@@ -4,7 +4,7 @@
     <strong>{{{ $errors->first('success') }}}</strong>
 </div>
 @elseif ($errors->any())
-{{{-- */ $errors = $errors->all() /*--}}}
+{{-- */ $errors = $errors->all() /*--}}
 <div class="alert alert-error alert-dismissable">
     <button class="close" type="button" data-dismiss="alert" aria-hidden="true">&times;</button>
     <strong>{{{ $errors[0] }}}</strong>
